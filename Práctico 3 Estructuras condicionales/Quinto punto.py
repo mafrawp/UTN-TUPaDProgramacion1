53f25eb7af587a0a46d1c17ae36410a4313af351
mafrawp <soy.hernan@hotmail.com>
2025-09-26 17:45:11 -0300

    Quinto Punto
    5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
    (incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
    pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
    pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
    de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
    como una lista o un string.

---
+++ Práctico 3 Estructuras condicionales/Quinto punto.py
@@ -0,0 +1,12 @@
+# 5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+# (incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+# pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+# pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+# de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+# como una lista o un string.
+contra=input("Ingrese su contraseña: ")
+carac=len(contra)
+if carac>=8 and carac<=14:
+    print("Ha ingresado una contraseña correcta.")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres.")
